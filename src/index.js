1293a5de800183aae15a94fe7ac9924ead1daf2a
Dagamu110 <44729041+Dagamu110@users.noreply.github.com>
2019-01-17 12:57:51 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ app.get('/app.js', ( req , res)  => {
 	res.sendFile(__dirname + '/public/app.js');
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
-	console.log("Server in port 3000");
-});
+var server = app.listen(8810);
 
 app.use(express.static('public'));
 
