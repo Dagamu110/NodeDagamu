8089700dea587910fdb22387370a0d090f218fa3
Dagamu110 <44729041+Dagamu110@users.noreply.github.com>
2019-01-17 19:05:17 -0300

    Update app.js

--- src/public/app.js
+++ src/public/app.js
@@ -1,14 +1,23 @@
 var socket = io.connect('https://dagamu.herokuapp.com');
 
 var btn_move = document.getElementById('move');
+var btn_send = document.getElementById('send');
+
+var input_chat = document.getElementById('chat');
 
 socket.on('recive', function(message){
-    console.log(message);
+    alert(message);
 });
 
 btn_move.addEventListener('click', function(){
     console.log('move');
     socket.emit('move', {
         message : 'move'
+    });
+});
+
+btn_send.addEventListener('click', function(){
+    socket.emit('move', {
+        message : input_chat.value
     });
 });
